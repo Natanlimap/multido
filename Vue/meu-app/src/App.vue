184ff3c2714474369603eec13dd2f596f81cf597
Natanlimap <natanlimap@gmail.com>
2020-09-23 00:17:59 -0300

    Versão vue do projeto iniciado

---
+++ Vue/meu-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+    <Homepage/>
+ </div>
+</template>
+
+<script>
+import Homepage from './Pages/HomePage/Homepage'
+export default {
+  name: 'App',
+  components: {
+    Homepage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/meu-app/src/Pages/HomePage/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>TESTE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Homepage"
+}
+</script>
+
+<style>
+
+</style>
